07723ff8f5ce07ebb206704d90d58441fcff2ed9
ck207636 <chandan2036482563@gmail.com>
2024-12-18 16:02:52 +0530

    Update RAGNAROK.py

--- RAGNAROK.py
+++ RAGNAROK.py
@@ -5,8 +5,6 @@ import requests
 import datetime
 import os
 
-from keep_alive import keep_alive
-keep_alive()
 # insert your Telegram bot tokSasen here
 bot = telebot.TeleBot('8146887226:AAEV8Sm5K4xOjqQLnSS3TgE8r6rIcUA56JQ')
 
